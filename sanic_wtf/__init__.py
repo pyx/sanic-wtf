dc9c28d77321780cd95d0c8529fd7a4cdc39d0a2
Philip Xu <pyx@xrefactor.com>
2017-04-27 18:45:42 -0400

    Bumped version number to 0.1.0

--- sanic_wtf/__init__.py
+++ sanic_wtf/__init__.py
@@ -4,7 +4,7 @@ from wtforms import Form
 from wtforms.csrf.session import SessionCSRF
 from wtforms.meta import DefaultMeta
 
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0'
 
 
 def to_bytes(text, encoding='utf8'):
